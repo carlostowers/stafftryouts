61837140055fe71cd3957e2803f685434eb4d3c9
carlostowers <catd04@gmail.com>
2026-07-30 11:30:51 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "cvvb-turnos-v1";
+var CACHE = "cvvb-turnos-v3";
 var CORE = ["./", "index.html", "manifest.json", "icon-192.png", "icon-512.png",
   "icon-maskable-512.png", "apple-touch-icon.png", "logo.png"];
 
